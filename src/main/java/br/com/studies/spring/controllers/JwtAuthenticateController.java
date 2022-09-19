f1e6b61e997fe3b862267fbe35af3f3947e22e96
Brendson <brendson.net@gmail.com>
2022-09-19 10:32:36 -0300

    feat: Hashed password comparison after applying PasswordEncoder with BCrypt

--- src/main/java/br/com/studies/spring/controllers/JwtAuthenticateController.java
+++ src/main/java/br/com/studies/spring/controllers/JwtAuthenticateController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,16 +18,19 @@ public class JwtAuthenticateController {
 
     @Autowired
     private UserRepository repository; 
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private List<User> usuarios = new ArrayList<>();
-
+    
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String createToken(@RequestBody User auth) {
         usuarios = repository.findAll();
 
         for (User user : usuarios) {
-            if (user.getUsername().equals(auth.getUsername()) && user.getPassword().equals(auth.getPassword())) {
-                return user.getUsername();
+            if (user.getUsername().equals(auth.getUsername()) && passwordEncoder.matches(auth.getPassword(), user.getPassword())) {
+                return user.getUsername() + "OK";
             } else {
                 return "erro";
             }
